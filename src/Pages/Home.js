dd215b234d45d3fa9a8e110035b8f431082486a1
inzamulhaque <mdihalif@gmail.com>
2022-05-22 00:37:34 +0600

    add page title component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PageTitle from '../Componests/PageTitle/PageTitle';
 import Banner from '../Componests/Home/Banner';
 
 const Home = () => {
     return (
         <>
+            <PageTitle title={"Home"} />
             <div className="container mx-auto">
                 <Banner />
             </div>
