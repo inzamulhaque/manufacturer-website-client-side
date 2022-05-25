cac1ae93a4a94faa90cb9210622cb75eae304f5a
inzamulhaque <mdihalif@gmail.com>
2022-05-26 04:02:24 +0600

    add skills part

---
+++ src/Componests/Protfolio/Skills.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Skills = () => {
+    return (
+        <>
+            <div className="mx-auto rounded-lg shadow-lg md:w-3/4 lg:w-1/2 my-5 bg-white p-3">
+                <h2 className="text-[28px] font-bold">Skills</h2>
+                <div className="px-2">
+                    <h4 className="text-[22px] font-medium">
+                        <span className="font-bold">FRONT END:</span>
+                        React, JavaScript (ES6), Css, Tailwind CSS, Bootstrap 5, HTML
+                    </h4>
+                    <h4 className="text-[22px] font-medium">
+                        <span className="font-bold">BACKEND:</span>
+                        JavaScript, Express Js, Node Js
+                    </h4>
+                    <h4 className="text-[22px] font-medium">
+                        <span className="font-bold">DataBase:</span>
+                        MongoDB
+                    </h4>
+                    <h4 className="text-[22px] font-medium">
+                        <span className="font-bold">Tools:</span>
+                        Git, Firebase, Heroku, NPM, VS Code, Figma
+                    </h4>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Skills;
--- src/Pages/Protfolio.js
+++ src/Pages/Protfolio.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PageTitle from '../Componests/PageTitle/PageTitle';
 import Header from '../Componests/Protfolio/Header';
+import Skills from '../Componests/Protfolio/Skills';
 
 const Protfolio = () => {
     return (
         <>
             <PageTitle title={"Protfolio"} />
             <Header />
+            <Skills />
         </>
     );
 };
