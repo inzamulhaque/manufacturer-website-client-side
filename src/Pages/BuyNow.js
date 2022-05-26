8a4ed68c60d91f603f19719ed1e31b27c9ea8b87
inzamulhaque <mdihalif@gmail.com>
2022-05-26 21:45:20 +0600

    maximum quentity error

--- src/Pages/BuyNow.js
+++ src/Pages/BuyNow.js
@@ -70,9 +70,9 @@ const BuyNow = () => {
             <div className="container mx-auto">
                 <h4 className="text-[25px] font-bold dark:text-white">{user?.displayName}</h4>
                 <h5 className="text-[22px] font-bold dark:text-white">{user?.email}</h5>
-                <div className="my-4">
-                    <div className="mx-auto w-full md:w-1/2 lg:w-1/3 rounded-lg shadow-lg p-3 dark:bg-white">
-                        <div className="w-full h-[250px] mb-2">
+                <div className="my-4 grid grid-cols-1 lg:grid-cols-2 gap-4">
+                    <div className="rounded-lg shadow-lg p-3 dark:bg-white">
+                        <div className="w-3/4 mx-auto h-[250px] mb-2">
                             <img src={img} alt="Tool Image" className='w-full h-full rounded-lg' />
                         </div>
                         <h4 className="text-[22px] font-bold">Name: {name}</h4>
@@ -80,7 +80,8 @@ const BuyNow = () => {
                         <p className="text-[20px] font-medium">Minimum Order Quantity: {minQty} unit</p>
                         <p className="text-[20px] font-medium">Available Quantity: {availableQty} unit</p>
                         <p className="text-[18px] font-normal">Description: {desc}</p>
-
+                    </div>
+                    <div className="rounded-lg shadow-lg p-3 dark:bg-white">
                         <form className='px-0 md:px-3 lg:px-5' onSubmit={onSubmit}>
                             <input type="number" name="qty" className="w-full border-b-2 border-black text-[22px] font-medium focus:outline-none rounded-lg px-2 my-2" placeholder='Enter Item Quantity' onChange={(e) => setQty(e.target.value)} value={qty} min={minQty} max={availableQty} required />
 
@@ -90,12 +91,18 @@ const BuyNow = () => {
                                     Please Order Minimum {minQty} Quantity
                                 </p>
                             }
+                            {
+                                parseInt(availableQty) < parseInt(qty) &&
+                                <p className="text-[20px] text-red-500 font-medium">
+                                    Please Order Maximum {availableQty} Quantity
+                                </p>
+                            }
 
                             <input type="number" name="phone" className="w-full border-b-2 border-black text-[22px] font-medium focus:outline-none rounded-lg px-2 my-2" placeholder='Enter Your Phone Number' required />
 
                             <input type="text" name="address" className="w-full border-b-2 border-black text-[22px] font-medium focus:outline-none rounded-lg px-2 my-2" placeholder='Enter Your Address' required />
 
-                            <button disabled={parseInt(qty) < parseInt(minQty)} type='submit' className="my-2 py-2 px-3 text-[20px] font-medium bg-blue-500 border-2 border-blue-500 text-white duration-300 ease-in-out hover:bg-transparent hover:text-blue-500 rounded-md">Order Now</button>
+                            <button disabled={(parseInt(qty) < parseInt(minQty) || (parseInt(availableQty) < parseInt(qty)))} type='submit' className="my-2 py-2 px-3 text-[20px] font-medium bg-blue-500 border-2 border-blue-500 text-white duration-300 ease-in-out hover:bg-transparent hover:text-blue-500 rounded-md">Order Now</button>
                         </form>
                     </div>
                 </div>
